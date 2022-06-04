7314b98b5fbd637bbf3a3186d4735cc69f4b3f7d
Yo sup <dhdytjq2@gamil.com>
2022-06-04 23:43:20 +0900

    nightPinLight event done

--- javascript/landingPage/landingPage.js
+++ javascript/landingPage/landingPage.js
@@ -2,18 +2,27 @@
 const landingPage = document.getElementById('landing')
 const centerBox = document.getElementById('backdropBox');
 
-console.log(window.scrollY);
+
+
 
 // console.log(maxPosition);
 
 
 
-let posY = landing.getBoundingClientRect().top;
+let posY = landingPage.getBoundingClientRect().top;
 
 let nowPosition = window.scrollY;
-let nextPage = main.offsetHeight
+let documentHeight = document.body.offsetHeight
+
+
+console.log(nowPosition);
+// 현재 스크롤의 위치
 
 console.log(posY);
+// 현재 스크롤의 위치(음수값)
+console.log(documentHeight);
+// 문서 전체의 높이
+
 
 
 function boxEvent (elems){
@@ -22,19 +31,35 @@ elems.style.width = '90vw';
 elems.style.transition = '0.5s';
 };
 
-
+function boxEventTwo (elems){
+  elems.style.opacity = 0.2;
+  elems.style.width = '80vw';
+  elems.style.transition = '0.5s';
+  };
 
 function scrollEvent(elems){
-window.scrollBy(0, nowPosition + nextPage);
+window.scrollBy({left : 0, top : 1466, behavior : 'smooth'});
 };
 
-export const scroll = window.addEventListener('wheel', (e)=>{
+function scrollEventUp(elems){
+  window.scrollBy({left : 0, top : -(documentHeight+posY), behavior : 'smooth'});
+  };
+
+
+
+export const scroll = landingPage.addEventListener('wheel', (e)=>{
 
   if(e.deltaY === 100){
   boxEvent(centerBox);
   scrollEvent(landingPage);
   }
+  if(e.deltaY === -100){
+    boxEventTwo(centerBox);
+    scrollEventUp(centerBox);
+  }
 });
+
+
 
 
 
--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,7 @@
 import { iconFunction } from "./javascript_source/hamberger.js";
 import { scroll } from "./landingPage/landingPage.js";
 
+import { light } from "./landingPage/nightPinLight.js";
 
 
 
