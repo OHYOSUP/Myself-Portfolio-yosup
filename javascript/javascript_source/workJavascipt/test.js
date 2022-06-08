1b146bc59e673f6cac4d7911d708072862144725
yosub <dhdytjq2@naver.com>
2022-06-08 18:00:10 +0900

    work page 따로스크롤 좀 해결하자...

---
+++ javascript/javascript_source/workJavascipt/test.js
@@ -0,0 +1,72 @@
+
+
+// const root = document.getElementById('root');
+// const right = document.getElementById('right');
+// const left = document.getElementById('left');
+// const container = document.getElementsByClassName('container');
+// console.log(root);
+
+
+
+
+// root.style.width = '100vw';
+// // root.style.height = '100vh';
+// root.style.overflow = 'hidden';
+// root.style.display = 'flex';
+
+// container.style.width = '100vw';
+// container.style.display = 'flex';
+
+// left.style.width = '30vw';
+// left.style.height = '100vh';
+
+
+
+
+
+
+// const createDiv = (parentElem, divName) =>{
+// var divName = document.createElement('div');
+// parentElem.appendChild(divName);
+// }
+
+
+// left.addEventListener('click', ()=>{
+
+// const leftBox = document.createElement('div');
+// left.appendChild(leftBox);
+
+// let i = 0;
+
+//   let boxHeight = setInterval(function(){
+//     i++;
+//     console.log(i);
+//     leftBox.style.height = i +'vh';
+//   },1)
+
+
+//   if(i === 100){
+//     clearInterval(boxHeight);
+//   };
+
+// leftBox.style.width = '30vw';
+// leftBox.style.backgroundColor = '#ccc';
+
+// });
+
+
+
+
+
+// console.log(left);
+
+
+// createDiv(root, left);
+
+
+// left.style.width = '30vw';
+// left.style.height = '100vh';
+
+// right.style.position = 'absolute';
+// right.style.overflow = 'hidden'
+
