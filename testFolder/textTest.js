5f6fae17b41fd6ed88de7212198d0401e0843041
Yo sup <dhdytjq2@gamil.com>
2022-06-12 21:55:19 +0900

    react install

---
+++ testFolder/textTest.js
@@ -0,0 +1,17 @@
+// const quote = document.getElementById('quote');
+
+// quote.innerHTML = '<span>' + quote.innerHTML.split('').join('</span><span>') + '</spam>';
+
+// quote.style.display = 'inline-flex';
+
+// console.log(quote);
+
+
+let mySplitText = new SplitText("#quote", {type:"words,chars", position : absolute});
+
+
+// function User(){
+// alert(new.target);
+// }
+
+// new User();
